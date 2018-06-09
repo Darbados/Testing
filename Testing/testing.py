e77f7d9485ddc74d0b7985407aba6af5f79b4cbc
petar.n <petar.n@delaspor.com>
2018-06-09 23:20:18 +0300

    adjustments

--- Testing/testing.py
+++ Testing/testing.py
@@ -1,22 +1,28 @@
 import random
-from Card_Deck.deck import Deck
-from Sorting.algorithms import SortAlgorithms
+from Testing.Card_Deck.deck import Deck
+from Testing.Sorting.algorithms import SortAlgorithms
 
-to_sort = [x for x in range(20)]
-random.shuffle(to_sort)
 
-sorting = SortAlgorithms(to_sort)
+def main():
+    to_sort = [x for x in range(20)]
+    random.shuffle(to_sort)
 
-print("Before sorting the array:\n{}".format(to_sort))
-sorting.merge_sort(sorting.to_sort)
-print("After sorting the array:\n{}".format(to_sort))
+    sorting = SortAlgorithms(to_sort)
 
-COLORS = ['D', 'H', 'C', 'S']
-CARDS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
-JOCKERS = ['RJ', 'BJ']
+    print("Before sorting the array:\n{}".format(to_sort))
+    sorting.merge_sort(sorting.to_sort)
+    print("After sorting the array:\n{}".format(to_sort))
 
-deck_obj = Deck(COLORS, CARDS, JOCKERS)
-deck_obj.build_deck()
+    COLORS = ['D', 'H', 'C', 'S']
+    CARDS = ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']
+    JOCKERS = ['RJ', 'BJ']
 
-print("Cards number in our deck is: {}".format(len(deck_obj.structure)))
-print("Random card from our deck: {}".format(deck_obj.get_card()))
+    deck_obj = Deck(COLORS, CARDS, JOCKERS)
+    deck_obj.build_deck()
+
+    print("Cards number in our deck is: {}".format(len(deck_obj.structure)))
+    print("Random card from our deck: {}".format(deck_obj.get_card()))
+
+
+if __name__ == '__main__':
+    main()
