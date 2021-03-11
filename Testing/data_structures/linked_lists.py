372e1a6029edaafa862aa740eec083198eb5866a
Petar Netev <pesho.netev@gmail.com>
2021-03-11 23:47:56 +0200

    Test remove duplicates from linked list

--- Testing/data_structures/linked_lists.py
+++ Testing/data_structures/linked_lists.py
@@ -38,13 +38,58 @@ class Dev:
         return f'{self.name} - {self.sector} - {self.level}'
 
 
-linked_devs = LinkedDevsList()
-linked_devs.append_dev('Angel', 'Frontend', level='Lead')
-linked_devs.append_dev('George', 'Frontend', level='Junior')
-linked_devs.append_dev('Vlado', 'Backend', level='Regular')
-linked_devs.append_dev('Misho', 'Backend', level='Mid - Reg')
-linked_devs.append_dev('Kaloyan', 'Backend', level='Junior')
-linked_devs.append_dev('Pesho', 'Backend', level='Manager, Lead')
+# linked_devs = LinkedDevsList()
+# linked_devs.append_dev('Angel', 'Frontend', level='Lead')
+# linked_devs.append_dev('George', 'Frontend', level='Junior')
+# linked_devs.append_dev('Vlado', 'Backend', level='Regular')
+# linked_devs.append_dev('Misho', 'Backend', level='Mid - Reg')
+# linked_devs.append_dev('Kaloyan', 'Backend', level='Junior')
+# linked_devs.append_dev('Pesho', 'Backend', level='Manager, Lead')
+#
+# linked_devs.print_list()
+# linked_devs.push_dev('Ivailo', 'Technology', level='CTO')
 
-linked_devs.print_list()
-linked_devs.push_dev('Ivailo', 'Technology', level='CTO')
+
+class IntNode:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+    def __str__(self):
+        return f'{self.value}'
+
+
+class LinkedIntegerList:
+    def __init__(self):
+        self.head = None
+
+    def print_list(self):
+        current_node = self.head
+        print_val = None
+        while current_node is not None:
+            if current_node.value != print_val:
+                print_val = current_node.value
+                print(print_val)
+            current_node = current_node.next
+
+    def clear_duplicate_nodes(self):
+        if not self.head.next:
+            return
+        current_node = self.head
+        while current_node:
+            next_unique_node = current_node.next
+            while next_unique_node and current_node.value == next_unique_node.value:
+                next_unique_node = next_unique_node.next
+            current_node.next = next_unique_node
+            current_node = next_unique_node
+
+
+linked_ints = LinkedIntegerList()
+linked_ints.head = IntNode(1)
+linked_ints.head.next = IntNode(1)
+linked_ints.head.next.next = IntNode(2)
+linked_ints.head.next.next.next = IntNode(2)
+linked_ints.head.next.next.next.next = IntNode(3)
+
+linked_ints.clear_duplicate_nodes()
+linked_ints.print_list()
