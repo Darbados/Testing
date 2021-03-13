e910f5690e785c617c419c670cf1f2914d2a2343
Petar Netev <pesho.netev@gmail.com>
2021-03-13 23:56:43 +0200

    Add unit test for smalles_difference solution

---
+++ Testing/algo_questions/tests/test_smallest_difference.py
@@ -0,0 +1,15 @@
+from unittest import TestCase
+
+from algo_questions.smallest_difference import get_smallest_difference
+
+
+class SmallestDifferenceTests(TestCase):
+    def test_smallest_difference(self):
+
+        array_one = [-1, 5, 10, 20, 28, 3]
+        array_two = [26, 134, 135, 15, 17]
+
+        self.assertEqual(
+            get_smallest_difference(array_one, array_two),
+            [28, 26]
+        )
