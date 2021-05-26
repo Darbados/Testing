ac01d105650e4f2b2d8a8a6d3e53513f45885640
Petar Netev <pesho.netev@gmail.com>
2021-05-26 21:55:10 +0300

    Finish longest peak solution

--- Testing/algo_questions/arrays/longest_peak.py
+++ Testing/algo_questions/arrays/longest_peak.py
@@ -2,34 +2,30 @@ test_array = [1, 2, 3, 3, 4, 0, 10, 6, 5, -1, -3, 2, 3]
 
 
 def get_longest_peak(array):
-    peaks = {}
-    max_peak_number = None
-    current_peak_key = None
-    current_idx = 0
-
-    # Need refactor for current_peak_key reset.
-    # Should remove peaks that are not actual peaks.
+    longest_peak = 0
+    current_idx = 1
 
     while current_idx < len(array) - 1:
-        if current_idx == 0:
-            if array[current_idx + 1] <= array[current_idx]:
-                current_idx += 1
-            else:
-                max_peak_number = array[current_idx]
-                current_peak_key = array[current_idx]
-                peaks[current_peak_key] = 1
-                current_idx += 1
-        else:
-            if max_peak_number < array[current_idx]:
-                max_peak_number = array[current_idx]
-                peaks[current_peak_key] += 1
-                current_idx += 1
-            elif max_peak_number == array[current_idx]:
-                current_idx += 1
-                current_peak_key = None
-            else:
-                peaks[current_peak_key] += 1
-                current_idx += 1
+        if array[current_idx - 1] < array[current_idx] > array[current_idx + 1]:
+            current_peak = 3
+            left_peak_side = current_idx - 2
+            right_peak_side = current_idx + 2
+            while left_peak_side >= 0:
+                if array[left_peak_side] < array[left_peak_side + 1]:
+                    current_peak += 1
+                    left_peak_side -= 1
+                else:
+                    break
+            while right_peak_side <= len(array) - 1:
+                if array[right_peak_side] < array[right_peak_side - 1]:
+                    current_peak += 1
+                    right_peak_side += 1
+                else:
+                    break
+            longest_peak = current_peak
+        current_idx += 1
+
+    return longest_peak
 
 
-get_longest_peak(test_array)
+print(get_longest_peak(test_array))
